d55b95ee52769e96327b4caa864b90d1001805ec
Markham Lee <markhamlee@outlook.com>
2020-09-26 02:21:39 -0700

    adding updated ReadMe + machine learning prep script

--- LendingClub/ml_data_prep.py
+++ LendingClub/ml_data_prep.py
@@ -1,3 +1,18 @@
+# this file performs pre-processing for machine learning in the form of feature
+# selection and removing correlated features.
+# It can also preform high level data cleaning, as there are functions that
+# would the user to remove columns that may have been useful for EDA,
+# but aren't useful for ML
+# Another purpose of this file is that it's not per se project specific,
+# I can pass datasets for multiple projects through this file and it
+# will generate dummy variables and delete unneeded features without
+# much customization
+# I didn't call the correlation functions in this particular version, it
+# was easier to call it in the notebook that for the learning modeling. It was
+# left in this file as a utility function in case I needed it later.
+
+# import needed packages
+
 import pandas as pd
 
 
@@ -7,48 +22,101 @@ pd.set_option('max_columns', None)
 pd.set_option('display.max_rows', None)
 
 
-# import data
+# data import function
 
-data_ml = pd.read_csv('data/LC_2015_clean(4).csv')
+def import_data(csv_file):
 
-data_ml.head(10)
+    # function to import the data from a CSV file
 
+    data_ml = pd.read_csv(csv_file)
 
-# select the columns we don't need and then remove them from the dataset
-
-columns = ['issue_d', 'addr_state', 'grade', 'earliest_cr_line',
-           'annual_inc', 'purpose', 'dti', 'application_type',
-           'total_pymnt', 'total_pymnt_inv', 'total_rec_prncp',
-           'total_rec_int', 'monthly_income', 'application_type',
-           'mths_since_last_delinq', 'num_tl_120dpd_2m', 'revol_bal',
-           'total_rec_late_fee', 'monthly_debt_payments',
-           'pub_rec_bankruptcies', 'updated_monthly_debt_payments',
-           'lost_principle', 'total_bal_ex_mort']
+    return(data_ml)
 
 
-data_ml.drop(columns, inplace=True, axis=1)
+def delete_columns(data, column_list):
 
-# add dummy variables to the data frame
+    data.drop(column_list, inplace=True, axis=1)
 
-data_ml = pd.get_dummies(data_ml)
-
-data_ml.head(20)
+    return data
 
 
-# rename the columns
+def add_dummies(data):
 
-data_ml.rename(columns={'loan_status_Fully Paid': 'paid'}, inplace=True)
-data_ml.rename(columns={'loan_status_Charged Off': 'default'}, inplace=True)
+    # this function generates dummy variables and then
+    # deletes the "default" column as we're going to be
+    # predicting "paid"
+    # this is the only function that's specific for the
+    # lending dataset
 
-data_ml
+    # add dummy variables to the data frame
+
+    data = pd.get_dummies(data)
+
+    data.rename(columns={'loan_status_Fully Paid': 'paid'}, inplace=True)
+    data.rename(columns={'loan_status_Charged Off': 'default'}, inplace=True)
+
+    columns = ['default']
+
+    data.drop(columns, inplace=True, axis=1)
+
+    return data
 
 
-# drop the default column
+def find_correlations(data, threshold):
 
-columns = ['default']
+    # identify correlated features and then remove them from the
+    # data 'data_ml' data frame
 
-data_ml.drop(columns, inplace=True, axis=1)
+    correlated_features = set()
 
-data_ml.to_csv(r'data/new_ml_data(paid-v6)_ALL.csv', index=False)
+    # drop the target variable (paid) and others that
+    # aren't just included for annualized return calculations
+    # not for machine learning
 
-print('file processing complete')
+    # columns to drop
+    columns = ['paid', 'total_payments', 'net_gain']
+
+    cordata = data.drop(columns, axis=1)
+
+    cor_matrix = cordata.corr()
+
+    for i in range(len(cor_matrix.columns)):
+        for j in range(i):
+            if abs(cor_matrix.iloc[i, j]) > threshold:
+                colname = cor_matrix.columns[i]
+                correlated_features.add(colname)
+
+    return list(correlated_features)
+
+
+
+def write_data(data, file_name):
+
+    # writes data to a csv when passed a data frame and a string with the
+    # file name
+
+    data.to_csv(file_name, index=False)
+
+    return(print('file processing complete'))
+
+
+def main():
+    ml_data = import_data('data/LC_2015_clean(4).csv')
+
+    # list of columns to delete
+    column_list = ['issue_d', 'grade', 'emp_length',
+                   'earliest_cr_line', 'addr_state',
+                   'annual_inc', 'purpose', 'dti',
+                   'total_pymnt', 'lost_principle',
+                   'total_pymnt_inv', 'monthly_income',
+                   'total_rec_late_fee', 'revol_bal',
+                   'monthly_debt_payments',
+                   'updated_monthly_debt_payments',
+                   'total_rec_int', 'total_rec_prncp']
+    data = delete_columns(ml_data, column_list)
+    data = add_dummies(data)
+    write_data(data, 'data/ml_ready_all_loans(new).csv')
+
+
+if __name__ == '__main__':
+    main()
